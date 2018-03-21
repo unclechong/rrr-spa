0a402e8c0f79034ad4108da844a281a312a67163
unclechong <unclechong>
2018-03-21 15:23:21 +0800

    修改假数据

--- app/api/datafusionApi.js
+++ app/api/datafusionApi.js
@@ -3,7 +3,25 @@ import sendPost from './commonApi';
 export default class datafusionApi {
 
     static async getTreeData(params={}){
-        const result = await sendPost('./test/tree.json', 'get', true, params);
+        // const result = await sendPost('./test/tree.json', 'get', true, params);
+        const result = {
+            "databaseSource": [
+                {
+                    "mongoId": "5aa64cb2848cd544ed491a4a",
+                    "sourceName": "数据库1"
+                }
+            ],
+            "documentSource": [
+                {
+                    "mongoId": "5aa64c7e848cd544ed491a48",
+                    "sourceName": "文档类型1"
+                },
+                {
+                    "mongoId": "5aa64c97848cd544ed491a49",
+                    "sourceName": "文档类型2"
+                }
+            ]
+        }
 
         const documentSource = result.documentSource.map((child, index)=>({title: child.sourceName, key: '0-'+index, value: child.mongoId}));
         const databaseSource = result.databaseSource.map((child, index)=>({title: child.sourceName, key: '1-'+index, value: child.mongoId}));
